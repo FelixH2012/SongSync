ac9abed44194a14d2a69a62165e2a759b63553d6
Felix <felixhans2007@gmail.com>
2023-08-09 12:14:11 +0200

    Icon, please dont strike this projects its just a placeholder lol

--- src/main/java/de/felix/songSync/SongSync.java
+++ src/main/java/de/felix/songSync/SongSync.java
@@ -5,10 +5,17 @@ import de.felix.songSync.elements.SFolderInfoBar;
 import de.felix.songSync.elements.SLinksList;
 import de.felix.songSync.elements.SMenuBar;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class SongSync {
+
+    private static final String ICON_LOCATION = "/icon_image.png"; //Just a placeholder don't care
 
     private static final int INITIAL_WIDTH = 600;
     private static final int INITIAL_HEIGHT = 400;
@@ -27,6 +34,17 @@ public class SongSync {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(INITIAL_WIDTH, INITIAL_HEIGHT);
 
+        final InputStream imgURL = SongSync.class.getResourceAsStream(ICON_LOCATION);
+
+        if (imgURL != null) {
+            try {
+                frame.setIconImage(ImageIO.read(imgURL));
+            } catch (IOException e) {
+                System.out.println("Unable to find icon on icon location: " + ICON_LOCATION);
+            }
+        } else {
+            System.out.println("Unable to find url path ");
+        }
         final SMenuBar menuBar = new SMenuBar(frame);
         frame.setJMenuBar(menuBar.getMenuBar());
 
