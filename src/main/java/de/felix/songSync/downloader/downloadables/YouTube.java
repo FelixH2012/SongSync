6321f0f195df867b9ea6bf741dcecca7020fb62e
Felix1337 <87606394+FelixH2012@users.noreply.github.com>
2023-08-12 10:55:57 +0100

    Patched boilerplate code in YouTube Downloader.

--- src/main/java/de/felix/songSync/downloader/downloadables/YouTube.java
+++ src/main/java/de/felix/songSync/downloader/downloadables/YouTube.java
@@ -17,71 +17,79 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class YouTube extends ILoader {
+    private final YtDlpFinder ytDlpFinder = new YtDlpFinder();
+
+    private String songName = "", downloadPath = "";
+
     public YouTube(String url, File file) {
         super(new URL(url, file.toPath()));
     }
 
-    private final YtDlpFinder ytDlpFinder = new YtDlpFinder();
-
     @Override
     public void download() {
         String youtubeVideoUrl = getUrl().getUrl();
+        String youtubeDlPath = getYoutubeDlPath();
 
-        String youtubeDlPath;
-
-        if (ytDlpFinder.getPathForApplication() != null) {
-            youtubeDlPath = ytDlpFinder.getPathForApplication().toAbsolutePath().toString();
-            System.out.println("Found YT-DLP on PC.");
-        } else {
-            //Todo download yt-dlp exe from server
-            System.out.println("Could not find YT-DLP on Pc, downloading.");
-            youtubeDlPath = "src/main/executables/yt-dlp.exe";
-        }
         try {
             final Quality quality = Quality.HIGHEST;
-            Path tempDirectory = Files.createTempDirectory("song-cache");
-            String tempOutputPath = tempDirectory.toString() + "/%(title)s.%(ext)s";
-            ProcessBuilder pb = new ProcessBuilder(youtubeDlPath, "--verbose", "--extract-audio", "--audio-format", "mp3", "--audio-codec", quality.name(), youtubeVideoUrl, "-o", tempOutputPath);
+            final Path tempDirectory = Files.createTempDirectory("song-cache");
+            final String tempOutputPath = tempDirectory.toString() + "/%(title)s.%(ext)s";
 
-            BufferedReader stdoutReader;
-            Process process = pb.start();
-            stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String stdoutLine;
-            Pattern progressPattern = Pattern.compile("\\d+\\.\\d+%");
-            Matcher progressMatcher;
+            final ProcessBuilder pb = createProcessBuilder(youtubeDlPath, quality, youtubeVideoUrl, tempOutputPath);
+            final Process process = pb.start();
 
-            Pattern pathPattern = Pattern.compile("\\[ExtractAudio\\] Destination: (.+)");
-            Matcher pathMatcher;
+            monitorDownloadProgress(process);
 
-            String downloadPath = "";
-
-            while ((stdoutLine = stdoutReader.readLine()) != null) {
-                progressMatcher = progressPattern.matcher(stdoutLine);
-                pathMatcher = pathPattern.matcher(stdoutLine);
-                if (pathMatcher.find()) {
-                    downloadPath = pathMatcher.group(1);
-                    System.out.println("Download path: " + downloadPath);
-                }
-                if (progressMatcher.find()) {
-                    String progress = progressMatcher.group();
-                    System.out.println("Download progress: " + progress);
-                } else {
-                    System.out.println(stdoutLine);
-                }
-            }
-
-
-            Path filePath = Paths.get(downloadPath);
-
-
-            Path fileName = filePath.getFileName();
-            String songName = fileName.toString();
-
-            if (!process.isAlive()) //todo find a better way for this autism
+            if (!process.isAlive())
                 FFMPEGLoudness.changeLoudnessFromFile(new File(downloadPath), new File(getUrl().getOutputPath() + "/" + songName));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getYoutubeDlPath() {
+        String youtubeDlPath;
+        if (ytDlpFinder.getPathForApplication() != null) {
+            youtubeDlPath = ytDlpFinder.getPathForApplication().toAbsolutePath().toString();
+            System.out.println("Found YT-DLP on PC.");
+        } else {
+            System.out.println("Could not find YT-DLP on Pc, downloading.");
+            youtubeDlPath = "src/main/executables/yt-dlp.exe";
+        }
+        return youtubeDlPath;
+    }
+
+    private ProcessBuilder createProcessBuilder(String youtubeDlPath, Quality quality, String youtubeVideoUrl, String tempOutputPath) {
+        return new ProcessBuilder(youtubeDlPath, "--verbose", "--extract-audio", "--audio-format", "mp3", "--audio-codec", quality.name(), youtubeVideoUrl, "-o", tempOutputPath);
+    }
+
+    private void monitorDownloadProgress(Process process) throws IOException {
+        final BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String stdoutLine;
+        final Pattern progressPattern = Pattern.compile("\\d+\\.\\d+%");
+        Matcher progressMatcher;
+        Pattern pathPattern = Pattern.compile("\\[ExtractAudio\\] Destination: (.+)");
+        Matcher pathMatcher;
+
+
+        while ((stdoutLine = stdoutReader.readLine()) != null) {
+            progressMatcher = progressPattern.matcher(stdoutLine);
+            pathMatcher = pathPattern.matcher(stdoutLine);
+            if (pathMatcher.find()) {
+                downloadPath = pathMatcher.group(1);
+                System.out.println("Download path: " + downloadPath);
+            }
+            if (progressMatcher.find()) {
+                String progress = progressMatcher.group();
+                System.out.println("Download progress: " + progress);
+            } else {
+                System.out.println(stdoutLine);
+            }
+        }
+
+        final Path filePath = Paths.get(downloadPath);
+        final Path fileName = filePath.getFileName();
+        songName = fileName.toString();
     }
 }
